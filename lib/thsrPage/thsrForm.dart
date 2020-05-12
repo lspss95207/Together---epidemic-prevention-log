216f3d9ef24621b3a97dbb17dca43cdccd5a2559
Samuel Huang <16890479+lspss95207@users.noreply.github.com>
2020-05-12 12:23:55 +0800

    use MOTC API

--- lib/thsrPage/thsrForm.dart
+++ lib/thsrPage/thsrForm.dart
@@ -355,25 +355,25 @@ class THSRFormState extends State<THSRForm> {
               '?\$top=1000&\$format=JSON';
       print(_serviceUrl);
       try {
-        // final response = await http.get(_serviceUrl,
-        //     headers: MOTC().GetAuthorizationHeader());
-        final response = await http.get(_serviceUrl, headers: {
-          'Accept':
-              'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-          'Accept-Encoding': 'gzip, deflate, br',
-          'Accept-Language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
-          'Cache-Control': 'max-age=0',
-          'Connection': 'keep-alive',
-          'DNT': '1',
-          'Host': 'ptx.transportdata.tw',
-          'Sec-Fetch-Dest': 'document',
-          'Sec-Fetch-Mode': 'navigate',
-          'Sec-Fetch-Site': 'none',
-          'Sec-Fetch-User': '?1',
-          'Upgrade-Insecure-Requests': '1',
-          'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
-        });
+        final response = await http.get(_serviceUrl,
+            headers: MOTC().GetAuthorizationHeader());
+        // final response = await http.get(_serviceUrl, headers: {
+        //   'Accept':
+        //       'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        //   'Accept-Encoding': 'gzip, deflate, br',
+        //   'Accept-Language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+        //   'Cache-Control': 'max-age=0',
+        //   'Connection': 'keep-alive',
+        //   'DNT': '1',
+        //   'Host': 'ptx.transportdata.tw',
+        //   'Sec-Fetch-Dest': 'document',
+        //   'Sec-Fetch-Mode': 'navigate',
+        //   'Sec-Fetch-Site': 'none',
+        //   'Sec-Fetch-User': '?1',
+        //   'Upgrade-Insecure-Requests': '1',
+        //   'User-Agent':
+        //       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+        // });
         print('Response status: ${response.statusCode}');
         // printWrapped('Response body: ${response.body}');
         var data = jsonDecode(response.body);
